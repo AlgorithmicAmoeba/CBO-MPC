cdd4264f66b9fb215dcd5907882834a008b58b3c
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-10-11 17:50:04 +0200

    Add DV to simulation

--- simulate.py
+++ simulate.py
@@ -12,7 +12,7 @@ G = utils.InternalDelay.from_tf_coefficients(num, den, delay)
 num = [[[-0.045], [-0.048], [0.004]], [[-0.23], [0.55], [-0.65]]]
 den = [[[8.1, 1], [11, 1], [8.5, 1]], [[8.1, 1], [10, 1], [9.2, 1]]]
 delay = [[0.5, 0.5, 1], [1.5, 0.5, 1]]
-Gpm = utils.InternalDelay.from_tf_coefficients(num, den, delay)
+G = utils.InternalDelay.from_tf_coefficients(num, den, delay)
 
 # Parameters
 T = 70
@@ -30,7 +30,7 @@ def Ysp_fun(t):
     return ans
 
 
-def Upm(t):
+def Udv(t):
     if t < 100:
         ans = numpy.array([0])
     else:
@@ -46,6 +46,6 @@ t_end = 200
 t_sim = numpy.linspace(0, t_end, t_end*10)
 
 sim = Simulate.SimulateMPC(G, N, M, P, dt_model, Q, R)
-df = sim.simulate(Ysp_fun, t_sim, save_data="test", live_plot=False)
+df = sim.simulate(Ysp_fun, t_sim, Udv=Udv, save_data="test", live_plot=False)
 
 plotting.plot_all(df)
