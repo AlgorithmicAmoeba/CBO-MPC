3c3201341ad19df6d1bcbd519dc39892854c04bb
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-10-11 12:03:09 +0200

    Add physical model disturbances

--- simulate.py
+++ simulate.py
@@ -5,9 +5,14 @@ import numpy
 
 # Slightly adapted from  H/W assignment 9
 num = [[[-0.045], [-0.048]], [[-0.23], [0.55]]]
-den = [[[8.1, 1], [11, 1]], [[8.1, 1], [10, 1]]]  # [[[8.1, 1], [11, 1]], [[8.1, 1], [10, 1]]]
+den = [[[8.1, 1], [11, 1]], [[8.1, 1], [10, 1]]]
 delay = [[0.5, 0.5], [1.5, 0.5]]
 G = utils.InternalDelay.from_tf_coefficients(num, den, delay)
+
+num = [[[-0.045], [-0.048], [0.004]], [[-0.23], [0.55], [-0.65]]]
+den = [[[8.1, 1], [11, 1], [8.5, 1]], [[8.1, 1], [10, 1], [9.2, 1]]]
+delay = [[0.5, 0.5, 1], [1.5, 0.5, 1]]
+Gpm = utils.InternalDelay.from_tf_coefficients(num, den, delay)
 
 # Parameters
 T = 70
@@ -24,14 +29,23 @@ def Ysp_fun(t):
         ans = numpy.array([1, 1])
     return ans.repeat(P)
 
+
+def Upm(t):
+    if t < 100:
+        ans = numpy.array([0])
+    else:
+        ans = numpy.array([10])
+    return ans
+
+
 Q = numpy.append(numpy.full(P, 100), numpy.full(P, 100))
 R = numpy.append(numpy.full(M, 1), numpy.full(M, 1))
 
 # Simulation setup
-t_end = 100
+t_end = 200
 t_sim = numpy.linspace(0, t_end, t_end*10)
 
-sim = Simulate.SimulateMPC(G, N, M, P, dt_model, Q, R)
-df = sim.simulate(Ysp_fun, t_sim, save_data="test")
+sim = Simulate.SimulateMPC(G, N, M, P, dt_model, Q, R, Gpm=Gpm)
+df = sim.simulate(Ysp_fun, t_sim, Upm=Upm, save_data="test", live_plot=False)
 
 plotting.plot_all(df)
