a03c0bf7eb3ba72d65c9cb7b70d713734195ef57
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-10-15 07:05:43 +0200

    Add weights

--- Tuner.py
+++ Tuner.py
@@ -4,11 +4,14 @@ import Simulate
 
 
 class Tuner:
-    def __init__(self, s: Simulate.SimulateMPC, Ysp, t_sim, error_method="ISE", **kwargs):
+    def __init__(self, s: Simulate.SimulateMPC, Ysp, t_sim, error_method="ISE", weights=None, **kwargs):
         self.s = s
         self.Ysp = Ysp
         self.t_sim = t_sim
         self.method = getattr(self, error_method)
+        self.weights = weights
+        if weights is None:
+            self.weights = numpy.ones(self.s.SM.outs)
         self.sim_kwargs = kwargs
 
         self.df = self.s.simulate(self.Ysp, t_sim, **kwargs)
@@ -25,7 +28,8 @@ class Tuner:
     def ISE(self):
         ts, es = self.get_errors()
         E = es**2
-        ans = scipy.integrate.trapz(E, ts)
+        integrals = scipy.integrate.trapz(E.T, ts)
+        ans = sum(integrals*self.weights)
         return ans
 
 
