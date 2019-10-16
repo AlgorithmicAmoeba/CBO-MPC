fd272ddc33b86bb3d2d8b45fe2c6289d3a4c82c7
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-10-16 06:10:06 +0200

    Change python version

--- tuning.py
+++ tuning.py
@@ -1,3 +1,6 @@
+import datetime
+
+import numpy
 import simulate
 import Tuner
 
@@ -7,4 +10,12 @@ Udv = simulate.Udv
 t_sim = simulate.t_sim
 
 tuner = Tuner.Tuner(sim, Ysp, t_sim, Udv=Udv)
-print(tuner.ISE())
+# initial = [list(simulate.Q) + list(simulate.R)]
+initial = [ 5.47606676e+02,  8.34201423e+02,  1.64844807e+03,  1.73601306e+01,
+        5.72445677e+01,  3.99744437e+00,  7.95707623e+01,  3.79307415e+01,
+        2.08173337e-01, -2.52593735e-03,  4.63515308e-02,  2.01792961e-01]
+bounds = [(1, 1000)]*len(initial)
+print(datetime.datetime.now())
+ans = tuner.tune(bounds, initial)
+print(datetime.datetime.now())
+print(ans)
