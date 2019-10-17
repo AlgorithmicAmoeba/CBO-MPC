5431d2d904b092907b7c4985e81b4dabd281f6e1
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-10-17 09:09:34 +0200

    Change tuning method

--- tuning.py
+++ tuning.py
@@ -7,15 +7,17 @@ import Tuner
 sim = simulate.sim
 Ysp = simulate.Ysp_fun
 Udv = simulate.Udv
-t_sim = simulate.t_sim
+# t_sim = simulate.t_sim
+t_sim = numpy.linspace(0, 100, 1000)
 
-tuner = Tuner.Tuner(sim, Ysp, t_sim, Udv=Udv)
-# initial = [list(simulate.Q) + list(simulate.R)]
-initial = [ 5.47606676e+02,  8.34201423e+02,  1.64844807e+03,  1.73601306e+01,
-        5.72445677e+01,  3.99744437e+00,  7.95707623e+01,  3.79307415e+01,
-        2.08173337e-01, -2.52593735e-03,  4.63515308e-02,  2.01792961e-01]
-bounds = [(1, 1000)]*len(initial)
-print(datetime.datetime.now())
-ans = tuner.tune(bounds, initial)
-print(datetime.datetime.now())
+tuner = Tuner.Tuner(sim, Ysp, t_sim, Udv=Udv, error_method="ISE", weights=[1, 5])
+
+initial = [100, 100, 5, 5]
+
+bounds = [(1, 150)]*len(initial)
+
+a = datetime.datetime.now()
+ans = tuner.tune(initial, bounds, simple_tune=True)
+b = datetime.datetime.now()
+print("Total time: ", b - a)
 print(ans)
